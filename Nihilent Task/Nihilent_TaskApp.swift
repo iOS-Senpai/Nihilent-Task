4539aabc0ea31dd90e9dc1bf95e639206f4a975c
Mukku Pradeep Kumar <mukkupradeep.kumar@absa.africa>
2023-07-20 09:42:02 +0530

    Initial Commit

---
+++ Nihilent Task/Nihilent_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nihilent_TaskApp.swift
+//  Nihilent Task
+//
+//  Created by AB020QU on 2023/07/20.
+//
+
+import SwiftUI
+
+@main
+struct Nihilent_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
